218f186ed5cc6150f7bec84cefbc65a763fe7813
Palle Westermann <pwe@tec.dk>
2025-04-20 14:13:02 +0200

    Here54_18_Authentication_WithOut_Roles

--- JwtAuthDotNet9/Controllers/AuthController.cs
+++ JwtAuthDotNet9/Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 ﻿using JwtAuthDotNet9.Entities;
 using JwtAuthDotNet9.Models;
 using JwtAuthDotNet9.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -39,6 +40,12 @@ namespace JwtAuthDotNet9.Controllers
             return Ok(token);
         }
 
+        [Authorize]
+        [HttpGet]
+        public IActionResult AuthenticatedOnlyEndpoint()
+        {
+            return Ok("You are authenticated!");
+        }
         
     }
 }
